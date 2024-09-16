ea0956f6f1d4fb0e8298b0379745d179a3d94dad
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2024-09-16 15:57:44 -0400

    feat-- utility functions

--- aqi/utils/functions.py
+++ aqi/utils/functions.py
@@ -1,11 +1,29 @@
+import os
+import pickle
+from tqdm import tqdm
+from pymer4.models import Lmer
+
 # Function to fit the series of predefined model formulas
 def fit_models(data, formulas):
     models = {}
-    for i, formula in enumerate(formulas):
-        model_name = f"model{i}"  # Generate model names dynamically
+    summaries = {}
+    
+    for i, formula in tqdm(enumerate(formulas), total=len(formulas), desc="Fitting Models"):
+        
+        model_name = f"model{i}"
         print(f"Fitting {model_name} with formula: {formula}")
-        models[model_name] = Lmer(formula, data=data, family='binomial').fit()
-    return models
+        
+        # Create the Lmer model object and store
+        # Fit the model
+        model = Lmer(formula, data=data, family='binomial')
+        fitted_model = model.fit()
+        
+        # Store the full model object and the summary
+        models[model_name] = model
+        summaries[model_name] = fitted_model
+        
+    return models, summaries
+
 
 # Save the dictionary of fitted models in the "outputs" folder
 def save_fitted_models(fitted_models, filename="fitted_models.pkl"):
@@ -20,6 +38,7 @@ def save_fitted_models(fitted_models, filename="fitted_models.pkl"):
     with open(filepath, 'wb') as f:
         pickle.dump(fitted_models, f)
     print(f"Models saved to {filepath}.")
+
 
 # Load the dictionary of fitted models from the "outputs" folder in the parent directory
 def load_fitted_models(filename="fitted_models.pkl"):
